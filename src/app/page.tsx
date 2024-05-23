96c3e67e8882bf91bdf14a58010899e91d140cbc
chedgo <chedgo@gmail.com>
2024-05-23 09:29:31 -0500

    initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,80 @@
-import Image from "next/image";
+import Image from 'next/image';
+import { ReactNode } from 'react';
 
-export default function Home() {
+const SectionTitle = ({ children }: { children: ReactNode }) => (
+  <h2 className="text-2xl font-semibold text-blue-800 mb-4">{children}</h2>
+);
+
+const Paragraph = ({ children }: { children: ReactNode }) => (
+  <p className="mb-4 text-gray-700">{children}</p>
+);
+
+const ListItem = ({ children }: { children: ReactNode }) => (
+  <li className="text-gray-600">{children}</li>
+);
+
+export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <section className="p-8 bg-white shadow-md rounded-lg">
+      <h1 className="mb-8 text-3xl font-bold tracking-tighter text-blue-800">
+        Diego Glusberg
+      </h1>
+      <Image
+        src={'/odd.png'}
+        width={500}
+        height={500}
+        alt="Diego Glusberg"
+        className="mb-8"
+      />
+      <Paragraph>
+        I&apos;m a full-stack engineer specializing in building AI-powered products.
+        My journey from a fine-dining chef to a tech professional has been
+        unconventional but rewarding. At Practica, which was acquired by
+        BetterUp in March 2024, I built our coaching marketplace and developed
+        an AI career coaching app. My work spanned from prompt engineering to
+        user interface design and data model creation.
+      </Paragraph>
+      <SectionTitle>What Drives Me</SectionTitle>
+      <Paragraph>
+        Kindness, efficiency and collaboration. Delivering results under tight
+        deadlines is second nature to me, thanks to my time in professional
+        kitchens. I&apos;m passionate about using technology to make a real impact on
+        people&apos;s lives.
+      </Paragraph>
+      <SectionTitle>Tech Experience</SectionTitle>
+      <div className="mt-2 text-gray-600">
+        <div className="font-semibold">Practica (Acquired by BetterUp)</div>
+        <div>
+          <em className="text-gray-500">
+            Founding Engineer and First Hire, Jan 2021 - March 2024
+          </em>
         </div>
+        <div className="mt-4">
+          Practica was a tight-knit team of just five people, which meant I wore
+          many hats and was involved in every aspect of the product.
+        </div>
+        <ul className="list-disc list-outside flex flex-col gap-3 mt-10 pl-5">
+          <ListItem>
+            AI Career Coaching App: Led development from concept to
+            implementation. Pioneered prompt engineering and AI algorithms for
+            personalized coaching.
+          </ListItem>
+          <ListItem>
+            Productivity and Code Quality: Streamlined team workflows, reducing
+            feature deployment time.
+          </ListItem>
+          <ListItem>
+            Full-Stack Solutions: Built scalable solutions with NextJS, React,
+            and Node. Integrated vector (Pinecone) and relational databases
+            (Postgres).
+          </ListItem>
+          <ListItem>
+            Strategic Decision-Making: Used data analytics and agile methodology
+            to shape product direction, shipping new updates and features
+            several times a week.
+          </ListItem>
+        </ul>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </section>
   );
 }
