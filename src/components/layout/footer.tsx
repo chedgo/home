77a211c62d630c95206c2b66f954395d83038abe
chedgo <chedgo@gmail.com>
2024-06-04 18:40:56 -0500

    add footer to theme

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -18,7 +18,7 @@ function ArrowIcon() {
 export default function Footer() {
   return (
     <footer className="mb-16">
-      <div className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+      <div className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-primary md:flex-row md:space-x-4 md:space-y-0">
       
         <div>
           <a
