e34a058476c87d055bbce70eb4f90b56fc99cb19
chedgo <chedgo@gmail.com>
2024-07-01 20:24:15 -0500

    align arrow

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -6,6 +6,7 @@ function ArrowIcon() {
       viewBox="0 0 12 12"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      className='relative bottom-[3px]'
     >
       <path
         d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
@@ -17,7 +18,7 @@ function ArrowIcon() {
 
 export default function Footer() {
   return (
-    <footer className="mb-16 max-w-screen-2xl">
+    <footer className="mb-16 max-w-screen-2xl font-tenon">
       <div className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-primary md:flex-row md:space-x-4 md:space-y-0 ml-4 justify-end mr-4">
         <div>
           <a
