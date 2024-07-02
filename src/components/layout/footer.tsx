17c92ab4d93f54a3e0647578b2e2b13e3a51d085
chedgo <chedgo@gmail.com>
2024-07-01 20:12:39 -0500

    update footer layout to include max width screen

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -17,7 +17,7 @@ function ArrowIcon() {
 
 export default function Footer() {
   return (
-    <footer className="mb-16">
+    <footer className="mb-16 max-w-screen-2xl">
       <div className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-primary md:flex-row md:space-x-4 md:space-y-0 ml-4 justify-end mr-4">
         <div>
           <a
