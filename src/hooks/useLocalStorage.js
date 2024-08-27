61b79c1ae9f6574d07ec5d68748b1cebc24c9132
chedgo <chedgo@gmail.com>
2024-08-27 16:27:22 -0500

    better localstorage hook

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,18 +1,31 @@
 import { useState, useEffect } from 'react';
 
 const useLocalStorage = (key, initialValue) => {
-  const [state, setState] = useState(() => {
-    const valueInLocalStorage = window.localStorage.getItem(key);
-    if (valueInLocalStorage) {
-      return JSON.parse(valueInLocalStorage);
-    }
-    return initialValue;
-  });
+  const [state, setState] = useState(initialValue);
 
   useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(state));
+    // Initialize state from localStorage
+    const valueInLocalStorage = window.localStorage.getItem(key);
+    if (valueInLocalStorage) {
+      setState(JSON.parse(valueInLocalStorage));
+    }
+
+    // Set up localStorage update
+    const updateLocalStorage = () => {
+      window.localStorage.setItem(key, JSON.stringify(state));
+    };
+    updateLocalStorage();
+
+    // Add event listener for storage changes
+    window.addEventListener('storage', updateLocalStorage);
+
+    // Clean up
+    return () => {
+      window.removeEventListener('storage', updateLocalStorage);
+    };
   }, [key, state]);
 
   return [state, setState];
 };
+
 export default useLocalStorage;
