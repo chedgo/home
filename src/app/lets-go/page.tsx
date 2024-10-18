ddeec7a0f68d2d1892f83bbd5f8d8e2ffd1c0586
chedgo <chedgo@gmail.com>
2024-10-18 08:25:57 -0500

    add activity picker

--- src/app/lets-go/page.tsx
+++ src/app/lets-go/page.tsx
@@ -27,27 +27,46 @@ export default function LetsGo() {
   //   };
   //   setDecks([defaultDeck]);
   // };
-
-
-const [selectedLocation, setSelectedLocation] = useState<Location>(DEFAULT_LOCATION);
+  const toggleActivity = (activity: string) => {
+    setSelectedActivities((prevActivities) => {
+      if (prevActivities.includes(activity)) {
+        return prevActivities.filter((a) => a !== activity);
+      } else {
+        return [...prevActivities, activity];
+      }
+    });
+  };
+  const activities = [
+    'take photos',
+    'eat',
+    'drink',
+    'read',
+    'learn',
+    'explore',
+    'shop',
+  ];
+  const [selectedLocation, setSelectedLocation] =
+    useState<Location>(DEFAULT_LOCATION);
+  const [selectedActivities, setSelectedActivities] = useState<string[]>([]);
   return (
     <div className="p-6 max-w-3xl mx-auto">
       <h1 className="text-3xl font-bold mb-6 text-center text-primary">
         Let&apos;s Go
       </h1>
-      
+
       <section className="mb-8 space-y-4 text-primary">
         <p>
-          Sometimes you just want to go somewhere and don&apos;t know where to go.
-          Sometimes we get caught wandering around the blockbuster longer than a
-          movie&apos;s runtime. Sometimes being able to do everything makes it hard
-          to do anything.
+          Sometimes you just want to go somewhere and don&apos;t know where to
+          go. Sometimes we get caught wandering around the blockbuster longer
+          than a movie&apos;s runtime. Sometimes being able to do everything
+          makes it hard to do anything.
         </p>
         <p>
           This tool was made to help make a choice and{' '}
           <span className="text-accent font-bold font-tenon-bold">
             move on with your life
-          </span>.
+          </span>
+          .
         </p>
         <p>
           A user can generate a deck of locations based on their preferences and
@@ -66,9 +85,30 @@ const [selectedLocation, setSelectedLocation] = useState<Location>(DEFAULT_LOCAT
         </p>
       </section>
       <div>
-        I am in <LocationAutocomplete location={selectedLocation} setLocation={setSelectedLocation} />
+        I am in{' '}
+        <LocationAutocomplete
+          location={selectedLocation}
+          setLocation={setSelectedLocation}
+        />
       </div>
-      
+      <div>I want to</div>
+      <div className="flex flex-wrap gap-2">
+        {activities.map((activity) => (
+          <div
+            className={`border border-primary rounded-full p-2 cursor-pointer w-fit text-primary ${
+              selectedActivities.includes(activity)
+                ? 'bg-primary text-white'
+                : 'bg-white'
+            }`}
+            key={activity}
+            onClick={() => {
+              toggleActivity(activity);
+            }}
+          >
+            {activity}
+          </div>
+        ))}
+      </div>
 
       {/* <PlacePicker
         decks={decks}
