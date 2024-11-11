644d54f8d2c1f23fb39c64671052a82a7b19fa28
chedgo <chedgo@gmail.com>
2024-11-11 14:12:46 -0600

    logic for round trip to get script

--- src/app/interview-practice/page.tsx
+++ src/app/interview-practice/page.tsx
@@ -1,4 +1,13 @@
+'use client';
+
+import { useState } from 'react';
+import { useGenerateScript } from '@/hooks/useGenerateScript';
 export default function InterviewPractice() {
+  const [jobDescription, setJobDescription] = useState('');
+  const [companyProfile, setCompanyProfile] = useState('');
+  const [resume, setResume] = useState('');
+
+  const { fetchScript, isLoading, script, isDoneLoading } = useGenerateScript();
   return (
     <div className="text-primary pl-4 lg:pl-10 pr-8">
       {/* instructions at the top*/}
@@ -13,28 +22,45 @@ export default function InterviewPractice() {
         <div>
           <div>please paste in the job post here:</div>
           <div>
-            <textarea></textarea>
+            <textarea
+              className="border-primary/50 border-2 focus:border-primary focus:outline-none"
+              value={jobDescription}
+              onChange={(e) => setJobDescription(e.target.value)}
+            ></textarea>
           </div>
           <div>
             please paste in a profile of the company here, anything you can find
             on the web will help:
           </div>
           <div>
-            <textarea></textarea>
+            <textarea
+              className="border-primary/50 border-2 focus:border-primary focus:outline-none"
+              value={companyProfile}
+              onChange={(e) => setCompanyProfile(e.target.value)}
+            ></textarea>
           </div>
           <div>please paste in your resume here:</div>
           <div>
-            <textarea></textarea>
+            <textarea
+              className="border-primary/50 border-2 focus:border-primary focus:outline-none"
+              value={resume}
+              onChange={(e) => setResume(e.target.value)}
+            ></textarea>
           </div>
         </div>
       </div>
       {/* button to generate a scripts */}
-      <div className="border-2 border-primary text-primary mt-8 p-2 w-fit">
+      <div
+        className={`border-2 border-primary text-primary mt-8 p-2 w-fit cursor-pointer ${
+          isLoading ? 'opacity-50' : ''
+        }`}
+        onClick={() => fetchScript(jobDescription, companyProfile, resume)}
+      >
         Generate Script
       </div>
       {/* when loaded, the script is displayed */}
       <div>
-        <div>script will go here</div>
+        <div>{script}</div>
       </div>
       {/* button to launch a the interview simulator, or regenerate the script */}
       <div className="flex gap-4">
